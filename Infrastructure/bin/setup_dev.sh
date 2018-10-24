c715955286cf84851227f793d8a95ec28e24cf64
antonios karkatsoulis <antonios.karkatsoulis-avaloq.com@bastion.bff6.example.opentlc.com>
2018-10-24 11:29:40 +0000

    Added Prod and Dev Setup

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -12,3 +12,34 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 # Code to set up the parks development project.
 
 # To be Implemented by Student
+
+oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-dev
+oc policy add-role-to-user view --serviceaccount=default -n ${GUID}-parks-dev
+
+DB_HOST="mongodb"
+DB_USERNAME="mongodb"
+DB_PASSWORD="mongodb"
+DB_NAME="parks"
+
+oc new-app mongodb-persistent --param MONGODB_DATABASE="${DB_NAME}" --param MONGODB_USER="${DB_USERNAME}" --param MONGODB_PASSWORD="${DB_PASSWORD}" --param MONGODB_ADMIN_PASSWORD="mongodb_admin_password" -n ${GUID}-parks-dev
+
+oc new-app -f ./Infrastructure/templates/mlbparks-dev-template.yaml \
+--param DB_HOST=${DB_HOST} \
+--param DB_PORT=27017 \
+--param DB_USERNAME=${DB_USERNAME} \
+--param DB_PASSWORD=${DB_PASSWORD} \
+--param DB_NAME=${DB_NAME} \
+--param GUID=${GUID} \
+-n ${GUID}-parks-dev
+
+oc new-app -f ./Infrastructure/templates/nationalparks-dev-template.yaml \
+--param DB_HOST=${DB_HOST} \
+--param DB_PORT=27017 \
+--param DB_USERNAME=${DB_USERNAME} \
+--param DB_PASSWORD=${DB_PASSWORD} \
+--param DB_NAME=${DB_NAME} \
+--param GUID=${GUID} \
+-n ${GUID}-parks-dev
+
+oc new-app -f ./Infrastructure/templates/parksmap-dev-template.yaml --param GUID=${GUID} -n ${GUID}-parks-dev
+
--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -13,3 +13,48 @@ echo "Setting up Parks Production Environment in project ${GUID}-parks-prod"
 # The Green services/routes need to be active initially to guarantee a successful grading pipeline run.
 
 # To be Implemented by Student
+
+oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-prod
+oc policy add-role-to-group system:image-puller system:serviceaccounts:${GUID}-parks-prod -n ${GUID}-parks-dev
+oc policy add-role-to-user view --serviceaccount=default -n ${GUID}-parks-prod
+
+DB_HOST="mongodb"
+DB_USERNAME="mongodb"
+DB_PASSWORD="mongodb"
+DB_NAME="parks"
+DB_REPLICASET="rs0"
+DB_KEYFILE_VALUE="12345678901234567890"
+
+# Create DB
+oc new-app -f ./Infrastructure/templates/mongodb-statefulset-template.yaml \
+--param MONGODB_ADMIN_PASSWORD=mongodb_admin_password \
+--param MONGODB_USER=${DB_USERNAME} \
+--param MONGODB_PASSWORD=${DB_PASSWORD} \
+--param MONGODB_DATABASE=${DB_NAME} \
+--param MONGODB_REPLICA_NAME=${DB_REPLICASET} \
+--param MONGODB_KEYFILE_VALUE=${DB_KEYFILE_VALUE} \
+--param VOLUME_CAPACITY=1Gi \
+--param MEMORY_LIMIT=1Gi \
+-n ${GUID}-parks-prod
+
+# Create apps
+oc new-app -f ./Infrastructure/templates/mlbparks-prod-template.yaml \
+--param DB_HOST=${DB_HOST} \
+--param DB_PORT=27017 \
+--param DB_USERNAME=${DB_USERNAME} \
+--param DB_PASSWORD=${DB_PASSWORD} \
+--param DB_NAME=${DB_NAME} \
+--param DB_REPLICASET=${DB_REPLICASET} \
+-n ${GUID}-parks-prod
+
+oc new-app -f ./Infrastructure/templates/nationalparks-prod-template.yaml \
+--param DB_HOST=${DB_HOST} \
+--param DB_PORT=27017 \
+--param DB_USERNAME=${DB_USERNAME} \
+--param DB_PASSWORD=${DB_PASSWORD} \
+--param DB_NAME=${DB_NAME} \
+--param DB_REPLICASET=${DB_REPLICASET} \
+-n ${GUID}-parks-prod
+
+oc new-app -f ./Infrastructure/templates/parksmap-prod-template.yaml -n ${GUID}-parks-prod
+
